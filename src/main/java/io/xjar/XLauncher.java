7e75716753f63f0d6a787b7df55cdb3a5801595d
tangwanchao <18284228392@163.com>
2020-06-24 19:15:54 +0800

    args 设置密码

--- src/main/java/io/xjar/XLauncher.java
+++ src/main/java/io/xjar/XLauncher.java
@@ -15,14 +15,29 @@ public class XLauncher implements XConstants {
     public final XDecryptor xDecryptor;
     public final XEncryptor xEncryptor;
     public final XKey xKey;
+    public static final String xjarAlgorithm = "xjarAlgorithm";
+    public static final String xjarKeySize = "xjarKeySize";
+    public static final String xjarIvSize = "xjarIvSize";
+    public static final String xjarPassword = "xjarPassword";
 
     public XLauncher(String... args) throws Exception {
         this.args = args;
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         String algorithm = scanner.nextLine();
         int keysize = Integer.parseInt(scanner.nextLine());
         int ivsize = Integer.parseInt(scanner.nextLine());
-        String password = scanner.nextLine();
+        String password = scanner.nextLine();*/
+        String algorithm = XConstants.DEFAULT_ALGORITHM;
+        int keysize = XConstants.DEFAULT_KEYSIZE;
+        int ivsize = XConstants.DEFAULT_IVSIZE;
+        String password = null;
+        String[] split;
+        for (String arg : args) {
+            if (arg.contains("--"+xjarPassword)){
+                split = arg.split("=");
+                password = split[1];
+            }
+        }
         this.xDecryptor = new XJdkDecryptor();
         this.xEncryptor = new XJdkEncryptor();
         this.xKey = XKit.key(algorithm, keysize, ivsize, password);
